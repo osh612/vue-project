30058a7d6bfc411682641ddbc7ab237ee548ee97
osh612 <osh612@gmail.com>
2021-12-16 18:30:37 +0900

    vue_side

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  
+  <div class="menu">
+    <a v-for="(a,i) in menu" :key="i">{{ a }}</a>
+  </div>
+  <Discount/>
+  <div class="item" v-for="(a,i) in product" :key="i">
+    <h4 @click="modal = true; selection = i">{{a.title}}</h4>
+    <div>
+      <img :src="a.image" @click="modal = true; selection = i">
+    </div>
+  </div>
+ 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import data from './assets/data';
+import Discount from './components/Discount.vue';
 
 export default {
   name: 'App',
+  data(){
+    return{
+      selection: 0,
+      product : data,
+      menu : ['Home', 'Products', 'About'],
+      modal: false,
+    }
+  },
+
+  methods : {
+  },
+
   components: {
-    HelloWorld
+    Discount
   }
 }
+
 </script>
 
 <style>
@@ -21,6 +47,54 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+.menu{
+  background-color: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+
+.menu a{
+  color: white;
+  padding: 10px;
+}
+
+body{
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg{
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+img{
+  width: 100%;
+}
+
+.exitmodal{
+  background: transparent;
+  border: transparent;
+  font-weight: bold;
+  font-size: 20px;
+}
+
+.item {
+  margin-top: 40px;
+  margin-bottom: 80px;
+}
+
+
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="black-bg" v-if="modal == true">
+    <div class="white-bg">
+      <h4>{{product[selection].title}}</h4>
+      <div>
+        <img :src="product[selection].image">
+      </div>
+      <p> {{ product[selection].price }}원</p>
+      <p>{{product[selection].content}}</p>
+      <div>
+        <button class="exitmodal" @click="modal = false">
+          X
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
